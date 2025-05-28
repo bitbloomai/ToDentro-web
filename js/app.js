667edc6c750e7bdbbc4be8fde7d851368994364e
Cyber_Gust <augusto.e.silva@hotmail.com>
2025-05-28 18:16:39 -0300

    1

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 // Configuração do Supabase
-const SUPABASE_URL = 'SUA_URL_SUPABASE_AQUI';
-const SUPABASE_KEY = 'SUA_ANON_KEY_AQUI';
+const SUPABASE_URL = 'https://zgaaubntvmdhygtjdmkl.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpnYWF1Ym50dm1kaHlndGpkbWtsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg0NjU5MDMsImV4cCI6MjA2NDA0MTkwM30.Kye4jS1DBAL8afBNVKP_4XMdA2lvC8Nl6_EqD4Y8OBE';
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 // Função para scroll suave até uma seção
