a20bf8da8c42c4177cb7d74e9cc4fc7669b666a0
Augusto Silva <gutomacos@MacBook-Pro-de-Augusto.local>
2025-05-29 22:54:44 -0300

    fs

--- js/app.js
+++ js/app.js
@@ -1,8 +1,3 @@
-// Configuração do Supabase
-const SUPABASE_URL = 'https://zgaaubntvmdhygtjdmkl.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpnYWF1Ym50dm1kaHlndGpkbWtsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg0NjU5MDMsImV4cCI6MjA2NDA0MTkwM30.Kye4jS1DBAL8afBNVKP_4XMdA2lvC8Nl6_EqD4Y8OBE';
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
-
 // Função para scroll suave até uma seção
 function scrollToSection(sectionId) {
     const section = document.getElementById(sectionId);
